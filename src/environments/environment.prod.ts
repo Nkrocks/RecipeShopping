d49ae8d0c97b441921519f4ce738cc2d96edc380
Nkrocks <nkadu34@gmail.com>
2019-10-09 18:34:19 +0530

    environment variable set and ready for deployment another ng Build --prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseAPIkey: "AIzaSyDdFdYfSk3TMk-YVTd5oo_fiHwc19mSImU"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseAPIkey: "AIzaSyDdFdYfSk3TMk-YVTd5oo_fiHwc19mSImU"
 };
 
 /*
